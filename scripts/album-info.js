3decdbde4dd4ca5b7072ff445bc45d24f5a779f8
gloreos <choi.gloria99@gmail.com>
2019-03-02 18:22:37 -0800

    Assignment 8 completed

--- scripts/album-info.js
+++ scripts/album-info.js
@@ -1,4 +1,6 @@
 {
   $("#album-title").text(album.title);
   $("img#album-cover-art").attr("src", album.albumArtUrl);
+  $("div.album-details").find(".artist").text(album.artist);
+  $("#release-info").text(album.releaseInfo);
 }
